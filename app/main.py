04e12825b900d4548a6aa70069f6e87285b92320
Kiran <kiran@appvintech.com>
2026-04-30 22:44:48 +0530

    feat/ implemented db loac on update using with_for_update and setup basic logger

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@
 from fastapi import FastAPI
 from app.api.booking import router as booking_router
 from app.models import *
+from app.core.logger import setup_logger
+
+setup_logger()
 
 app = FastAPI()
 app.include_router(booking_router)
