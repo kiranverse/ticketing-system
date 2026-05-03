856b0cd737bddcbf7115a83a39734beee7143b32
Kiran <kiran@appvintech.com>
2026-05-03 19:53:25 +0530

    implemented decode access token ang get current user using token

--- app/core/deps.py
+++ app/core/deps.py
@@ -3,8 +3,11 @@ from jose import jwt
 from sqlalchemy.orm import Session
 from app.core.database import SessionLocal
 from app.models.user import User
-from app.core.jwt import SECRET_KEY, ALGORITHM
-from fastapi.security import HTTPBearer
+from app.core.jwt import SECRET_KEY, ALGORITHM, decode_access_token
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from app.core.logger import get_logger
+
+logger = get_logger(__name__)
 
 security = HTTPBearer()
 
@@ -18,10 +21,32 @@ def get_db():
     finally:
         db.close()
 
-def get_current_user(token=Depends(security), db: Session = Depends(get_db)):
-    try:
-        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
-        user = db.query(User).filter(User.id == payload["user_id"]).first()
-        return user
-    except:
-        raise HTTPException(status_code=401, detail="Invalid token")
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    # 1. Extract token
+    token = credentials.credentials
+
+    # 2. Decode token
+    payload = decode_access_token(token)
+
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    # 3. Extract user_id
+    user_id = payload.get("user_id")
+
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
+
+    # 4. Fetch user from DB
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    logger.info(f"Decoded user_id={user.id}")
+    db.close()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # 5. Return user
+    return user
--- app/core/jwt.py
+++ app/core/jwt.py
@@ -1,4 +1,4 @@
-from jose import jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
 SECRET_KEY = "secret"
@@ -8,3 +8,10 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     to_encode["exp"] = datetime.utcnow() + timedelta(hours=2)
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
